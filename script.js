783c3952cadd90b2d9ab5c9380a5123c93ea6613
julius <59978068+smartjulger@users.noreply.github.com>
2026-02-12 09:44:41 +0100

    Refactor API code and improve formatting

--- script.js
+++ script.js
@@ -56,8 +56,9 @@ function toonPersoon() {
   lijst.innerHTML = persoonHTML;
 }
 
-
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
 function randomPersoon() {
   huidigeIndex = Math.floor(Math.random() * alleMensen.length);
   toonPersoon();
+
 }
